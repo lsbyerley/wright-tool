f85bfd3e3faf329e1d2598f84bd148e1f8642e94
lsbyerley <lsbyerley@gmail.com>
2017-05-31 13:19:41 -0400

    introduced barba page transitions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,37 @@
-//window.jQuery = require('jquery');
 window.cash = require('cash-dom');
+var Barba = require('barba.js');
 
 (function(window, document, $) {
 	'use strict';
 
 	window.wtinc = (window.wtinc) ? window.wtinc : {};
 
-	//console.log( $(window).width() );
-
+	// Toggles the responsive nav
 	var $toggle = $('#header .nav-toggle');
 	var $menu = $('#header .nav-menu');
-
 	$toggle.on('click', function() {
 		$(this).toggleClass('is-active');
 		$menu.toggleClass('is-active');
+	});
+
+	// Barba.js page transitions
+	Barba.Pjax.start();
+	Barba.Dispatcher.on('linkClicked', function(HTMLElement, MouseEvent) {
+		var link = $(HTMLElement);
+		if (link.hasClass('nav-item')) {
+			$('.nav-item').removeClass('is-active');
+			link.addClass('is-active');
+		}
+	});
+	Barba.Dispatcher.on('newPageReady', function(currentStatus, prevStatus, HTMLElementContainer, newPageRawHTML) {
+		// For Google Analytics tracking, make sure ga() exists first.
+        if ( typeof ga === "function" ) {
+            ga('set', {
+                page: window.location.pathname,
+                title: document.title
+            });
+            ga('send', 'pageview');
+    	}
 	});
 	
 })(window, document, window.cash);
